1dd46ef579b4d05f6f95214fc32a56b67907042e
Mark Spratt <panzerlink@gmail.com>
2025-02-02 12:59:29 -0600

    Add admin per astro docs. we'll mod based on our configuration next

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,6 +11,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: SITE.website,
+  output: "server",
   integrations: [
     tailwind({
       config: {
